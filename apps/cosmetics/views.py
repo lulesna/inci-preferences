0bf759dde5e4581ea6efab9815675c10c0982315
Lucja Lesna <lesna.lucja@gmail.com>
2025-12-14 12:35:23 +0100

    dodanie filtrowania kosmetykow

--- apps/cosmetics/views.py
+++ apps/cosmetics/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Cosmetic
 from .serializers import CosmeticSerializer
 
@@ -6,3 +7,6 @@ from .serializers import CosmeticSerializer
 class CosmeticViewSet(viewsets.ModelViewSet):
     queryset = Cosmetic.objects.all()
     serializer_class = CosmeticSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['main_category', 'subcategory', 'brand']
+    search_fields = ['name', 'brand']
